85ed88f8fee77ee542289ce15a3f67d32a36b91b
hahnharper <57683179+hahnharper@users.noreply.github.com>
2020-03-07 12:46:03 -0700

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,11 @@
+-- DROP TABLE pandemics;
+
+CREATE TABLE pandemics (
+    Country VARCHAR (30) NOT NULL,
+    Number_of_Cases_SARS_2003 VARCHAR,
+    Number_of_Deaths_SARS_2003 VARCHAR,
+	Number_of_Cases_TB_2007 VARCHAR,
+	Number_of_Deaths_TB_2007 VARCHAR,
+	Number_of_Cases_H1N1_2009 VARCHAR,
+	Number_of_Deaths_H1N1_2009 VARCHAR
+);
